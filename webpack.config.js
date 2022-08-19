a6a2e0889ccae8b82c73620560f1bcb37fb5bb7e
Jon Giarla <jonathangiarla@gmail.com>
2022-08-19 07:17:42 -0400

    Basic skeleton website set up through js modules.

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -5,5 +6,25 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif|svg|bmp)$/i,
+                type: 'asset/resource',
+            },
+        ],
+
     },
 };
